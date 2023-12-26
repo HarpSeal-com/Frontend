ae90b8f07678e189dd84a75534fdf2122cf08bd7
Emad Gasser <omda.g@outlook.com>
2023-12-26 16:25:43 +0000

    node modules

---
+++ node_modules/@coreui/chartjs/js/index.esm.js
@@ -0,0 +1,8 @@
+/**
+ * --------------------------------------------------------------------------
+ * Custom Tooltips for Chart.js (v3.1.2): index.esm.js
+ * Licensed under MIT (https://github.com/@coreui/coreui-chartjs/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+export { default as customTooltips } from './src/custom-tooltips.js'
---
+++ node_modules/@coreui/chartjs/js/index.umd.js
@@ -0,0 +1,13 @@
+/**
+ * --------------------------------------------------------------------------
+ * Custom Tooltips for Chart.js (v3.1.2): index.umd.js
+ * Licensed under MIT (https://github.com/@coreui/coreui-chartjs/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+// eslint-disable-next-line no-unused-vars
+import customTooltips from './src/custom-tooltips.js'
+
+export default {
+  customTooltips
+}
